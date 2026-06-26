5e18b6838d861e62c324a69991a7b7ab2fc02114
what-sapp <sapp15425@gmail.com>
2026-06-26 14:18:31 +0300

    bina

---
+++ lib/Utils/browser-utils.d.ts
@@ -0,0 +1,9 @@
+export namespace Browsers {
+    function ubuntu(browser: any): any[];
+    function macOS(browser: any): any[];
+    function baileys(browser: any): any[];
+    function windows(browser: any): any[];
+    function appropriate(browser: any): any[];
+}
+export function getPlatformId(browser: any): string;
+//# sourceMappingURL=browser-utils.d.ts.map
